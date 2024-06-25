1db8f1e05c5b7ce4210ce79c1cb275c34f7f0a32
MI AKASH <121384260+MIAKASH19@users.noreply.github.com>
2024-06-25 19:57:25 +0600

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Home from './Pages/Home'
 const App = () => {
   return (
     <div className='bg-red-500 inline-block'>
-      <Homegit push origin main
- />
+      <Home />
     </div>
   )
 }
